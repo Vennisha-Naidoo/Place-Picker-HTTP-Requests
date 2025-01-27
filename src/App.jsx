f5174ee80129a583a054aec031bed2fe9c633368
Vennisha-Naidoo <vennisha31@gmail.com>
2025-01-28 01:54:30 +0200

    Optimistic Updating and Handling Errors

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import { updateUserPlaces } from './http.js';
+import ErrorPage from './components/Error.jsx';
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -37,7 +39,9 @@ function App() {
     try {
       await updateUserPlaces([selectedPlace, ...userPlaces]);
     } catch (error) {
-      
+      //"optimistic updating"
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({ message: error.message || "Failed to update places." })
     }
     
 
@@ -51,8 +55,24 @@ function App() {
     setModalIsOpen(false);
   }, []);
 
+  function handleError() {
+    setErrorUpdatingPlaces(null);
+  }
+
   return (
     <>
+    
+      <Modal open={ errorUpdatingPlaces } onClose={ handleError }>
+        {
+          errorUpdatingPlaces &&
+          <ErrorPage 
+            title="Oops! An error has occurred." 
+            message={ errorUpdatingPlaces.message } 
+            onConfirm={ handleError }
+          />
+        }
+      </Modal>
+
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
